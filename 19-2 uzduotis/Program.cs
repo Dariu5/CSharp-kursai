5bea6350c05b7d86993b5dc086bdbfbc80f087e0
Darius <radioool@gmail.com>
2018-05-21 21:13:22 +0300

    exception

--- 19-2 uzduotis/Program.cs
+++ 19-2 uzduotis/Program.cs
@@ -3,13 +3,138 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace _19_2_uzduotis
 {
+    class SkirtingiTipai<T, K,A> where T:IEnumerable<int> where A:Program
+    {
+
+   public List<T> vienasTipas { get; set; }
+   public List<T> antrasTipas { get; set; }
+   public List<T> treciasTipas { get; set; }
+    }
+
+
+
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            string path = null;
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+
+            {
+
+                path = fbd.SelectedPath;
+
+            }
+
+            System.IO.StreamReader reader = new System.IO.StreamReader(path + "//data.csv");
+
+            string line;
+
+            List<List<int>> sarasas = new List<List<int>>();
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] data = line.Split(';');
+                List<int> tempData = new List<int>();
+
+                foreach (var item in data)
+
+                {
+                    tempData.Add(int.Parse(item));
+
+                }
+
+                sarasas.Add(tempData);
+            }
+
+
+            foreach (var item in sarasas)
+
+            {
+                Console.WriteLine("mokinio vidurkis yra{0:0.00}" , item.Average());
+
+
+            }
+
+
+            var laikinoji_kopija = sarasas.ToArray();
+
+            for (int i = 0; i < laikinoji_kopija[0].Count; i++)
+            {
+
+
+                int suma = 0;
+                for (int j = 0; j < laikinoji_kopija.Length; j++)
+                {
+                    if (laikinoji_kopija[j].Count > i)
+                        suma += laikinoji_kopija[j].ElementAt(i);
+                   
+                }
+
+                Console.WriteLine("Kontrolinio vidurkis yra {0:0.00}", (double)suma / laikinoji_kopija.Length);
+            }
+
+
+            /*
+        for (int i = 0; i < sarasas.Count; i++)
+        {
+            int suma = 0;
+            foreach (var item in sarasas)
+
+            {
+                suma += item.ElementAt(i);
+
+            }
+
+            Console.WriteLine("Kontrolinio vidurkis yra {0:0.00}" , (double)suma / sarasas.Count());
+
+        }
+
+*/
+
+
+
+            List<List<int>> surikiuotaInformacija = new List<List<int>>();
+            while (sarasas.Count != 0)
+            {
+                double max = double.MinValue;
+                List<int> maxMember = new List<int>();
+
+                foreach (var item in sarasas)
+                {
+
+                    if (max < item.Average())
+                    {
+
+                        max = item.Average();
+                        maxMember = item;
+
+                    }
+
+                }
+                surikiuotaInformacija.Add(maxMember);
+                sarasas.Remove(maxMember);
+
+            }
+
+
+            foreach (var item in surikiuotaInformacija)
+            {
+                Console.WriteLine("Mokinio vidurkis {0} ", item.Average());
+            }
+
+
+
+            List<SkirtingiTipai<IEnumerable<int>, string,Program>> kazkas = new List<SkirtingiTipai<IEnumerable<int>, string, Program>>();
+
         }
     }
 }
---
+++ 20-1 uzduotis koldunai/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20_1_uzduotis_koldunai
+{
+
+    public class Dalyvis
+
+    {
+        public string Vardas { get; }
+        public int Kiekis { get; }
+        public int Riba { get; }
+        public int Diskvalifikuotas { get; set; }
+
+        public Dalyvis(string Vardas, int Kiekis, int Riba)
+        {
+            this.Vardas = Vardas;
+            this.Kiekis = Kiekis;
+            this.Riba = Riba;
+        }
+
+     }
+
+
+
+    class Program
+    {
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+
+            int laikas = 1;
+
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            string path = null;
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+
+            {
+
+                path = fbd.SelectedPath;
+
+            }
+
+            System.IO.StreamReader reader = new System.IO.StreamReader(path + "//dalyviai.csv");
+
+            string line;
+
+
+            List<Dalyvis> dalyviai = new List<Dalyvis>();
+
+
+
+            while ((line = reader.ReadLine()) != null)
+            {
+
+                try
+                {
+                    string[] data = line.Split(';');
+
+                    Dalyvis dalyvis = new Dalyvis(data[0], int.Parse(data[1]), int.Parse(data[2]));
+
+
+                    if (dalyvis.Kiekis > dalyvis.Riba)
+                        throw new PersivalgeKodunuException(dalyvis.Kiekis);
+                    dalyviai.Add(new Dalyvis(data[0], int.Parse(data[1]), int.Parse(data[2])));
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Persivalge");
+                }
+
+
+
+
+            }
+
+
+            double max = double.MinValue;
+
+            Dalyvis max_dalyvis = new Dalyvis(null, 0, 0);
+
+            foreach (var item in dalyviai)
+            {
+
+                Console.WriteLine("{0} valgymo greitis: {1}", item.Vardas, ((double)item.Kiekis / laikas).ToString());
+
+                if (item.Kiekis > max)
+                {
+                    max = item.Kiekis;
+                    max_dalyvis = item;
+
+                }
+
+
+
+            }
+            Console.WriteLine("laimejo {0}, suvalges {1}", max_dalyvis.Vardas, max_dalyvis.Kiekis.ToString());
+
+
+
+
+        }
+
+
+    }       
+
+        class PersivalgeKodunuException : Exception
+
+        {
+
+            public int KiekSuvalge { get; set; }
+
+            public PersivalgeKodunuException(int KiekSuvalge)
+            {
+
+                this.KiekSuvalge = KiekSuvalge;
+            }
+        }
+
+   
+}
