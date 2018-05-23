88bce06b32cdd30c3bd852943aec57d35f32498b
Dariu5 <radiocool@gmail.com>
2018-05-23 20:03:10 +0300

    Events

---
+++ 22-2 pvz2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _22_2_pvz2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += MetaMessageBox;
+        }
+
+        private void MetaMessageBox(object sender, EventArgs e)
+
+        {
+
+            MessageBox.Show("Issoko langas");
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Labas";
+        }
+    }
+}
