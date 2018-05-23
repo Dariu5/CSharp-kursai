6672b4bb7e0b56697481c757c65672380acb9f4f
Darius <radioool@gmail.com>
2018-05-23 19:40:42 +0300

    Event

---
+++ 22-1 pvz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_1_pvz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            VideoEncoder videoencoderis = new VideoEncoder();
+            MessageService messageService = new MessageService();
+
+            videoencoderis.VideoEncoded += messageService.OnVideoEncoded;
+
+            videoencoderis.Encode();
+
+        }
+    }
+
+    class MessageService
+    {
+
+        public void OnVideoEncoded(object sender, EventArgs eventArgs)
+
+        {
+
+            Console.WriteLine("Message Sevice: Siunciame sms...");
+
+
+        }
+
+
+
+
+    }
+}
---
+++ 22-1 pvz/VideoEncoder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_1_pvz
+{
+    class VideoEncoder
+    {
+        //1 Apsirašyti delegatą
+        //2-susikurti įvykį pagal delegatą
+        //3 Sugeneruoti įvykį
+
+
+        public delegate void VideoEncodedEventHandler(object sender, EventArgs eventArgs); /*Kontraktas*/
+        public event VideoEncodedEventHandler VideoEncoded;
+
+
+        public void Encode()
+
+        {
+
+            Console.WriteLine("Koduojam video");
+            System.Threading.Thread.Sleep(3000);
+            OnVideoEncoded();
+
+        }
+
+        protected virtual void OnVideoEncoded()
+        {
+
+            if (VideoEncoded != null)
+            {
+
+                VideoEncoded(this, EventArgs.Empty);
+
+            }
+
+        }
+
+
+    }
+}
