4739e3bccf4242de695b129335b453202ef1aaf0
Dariu5 <radiocool@gmail.com>
2018-05-23 20:39:20 +0300

    Events

---
+++ 22-22 uzduotis/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_22_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Ivykis ivikis = new Ivykis();
+
+            ivikis.IvikisDone += consoleprint;
+            ivikis.IvikisDone += txtprint;
+
+            ivikis.Procesas();
+
+
+
+
+
+             void consoleprint (object sender, EventArgs eventArgs)
+
+            {
+
+                Console.WriteLine("Spausdinu");
+
+            }
+
+
+            void txtprint(object sender, EventArgs eventArgs)
+
+            {
+
+                StreamWriter sw = new StreamWriter("test.txt");
+
+                sw.WriteLine("Ivykis buvo");
+                sw.Flush();
+
+
+
+            }
+
+
+
+
+        }
+    }
+}
---
+++ 22-22 uzduotis/ivikis.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_22_uzduotis
+{
+    class Ivykis
+    {
+
+        public delegate void IvikisEventHandler(object sender, EventArgs eventArgs); /*Kontraktas*/
+        public event IvikisEventHandler IvikisDone;
+
+
+        public void Procesas()
+
+        {
+
+            Console.WriteLine("Procesas done");
+            OnIvikisDone();
+
+        }
+        
+
+
+
+        protected virtual void OnIvikisDone()
+        {
+
+            if (IvikisDone != null)
+            {
+
+                IvikisDone(this, EventArgs.Empty);
+
+            }
+
+        }
+
+    }
+}
